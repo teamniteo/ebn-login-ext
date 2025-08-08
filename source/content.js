f1e8b96a814fd231f53ab86c3c5750b4b56ec954
majarovtar <rovtar.maja@gmail.com>
2025-08-08 11:06:35 +0200

    Cleanup Mixpanel

--- source/content.js
+++ source/content.js
@@ -1,10 +1,8 @@
 import {default as injectLogin} from './login';
 import {default as injectLink} from './link';
-import {default as injectUser} from './mix';
 
 /* istanbul ignore next */
 document.addEventListener('DOMContentLoaded', () => {
 	injectLogin();
 	injectLink();
-	injectUser();
 });
--- source/mix.js
+++
@@ -1,8 +0,0 @@
-
-export default async function injectUser() {
-	if (document.location.host === 'app.easyblognetworks.com') {
-		const s = document.createElement('script');
-		s.src = chrome.extension.getURL('track.js');
-		(document.head || document.documentElement).append(s);
-	}
-}
